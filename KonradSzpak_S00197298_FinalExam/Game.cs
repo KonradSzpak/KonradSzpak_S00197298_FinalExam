282ed21d892a615d33458e43f19cbeb92f8cb298
Konrad Szpak <konradszpak67@gmail.com>
2021-05-11 09:40:54 +0100

    properties

---
+++ KonradSzpak_S00197298_FinalExam/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonradSzpak_S00197298_FinalExam
+{
+    class Game
+    {
+        public string Name { get; set; }
+        public int MetacriticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+        public string GameImage { get; set; }
+
+    }
+}
